cb6d675af318813277644dbf75dddd7a003dc706
David Whitney <david@davidwhitney.co.uk>
2013-02-26 14:22:18 +0000

    tests were created in the wrong place. oops.

---
+++ ReallySimpleEventing.Test.Unit/ReallySimpleEventingTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using ReallySimpleEventing.EventHandling;
+
+namespace ReallySimpleEventing.Test.Unit
+{
+    [TestFixture]
+    public class ReallySimpleEventingTests
+    {
+        [Test]
+        public void AA()
+        {
+            var eventBus = ReallySimpleEventing.CreateRealTimeEventBus();
+            
+            eventBus.Raise(new MyEvent());
+        }
+    }
+
+    public class MyEvent
+    {
+        public string EventData { get { return "hi"; } }
+    }
+
+    public class DoSomethingWhenMyEventHappens : IHandle<MyEvent>
+    {
+        public void Handle(MyEvent @event)
+        {
+            
+        }
+    }
+}
