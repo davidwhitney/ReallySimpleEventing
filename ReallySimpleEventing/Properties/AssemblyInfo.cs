b61117deb9036e1202b6b75dc3770fe791820580
David Whitney <david@davidwhitney.co.uk>
2013-02-27 17:57:57 +0000

    added nuspec and package batch

--- ReallySimpleEventing/Properties/AssemblyInfo.cs
+++ ReallySimpleEventing/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReallySimpleEventing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tiny eventing system for adding domain events to your application. Supports running tasks both sync and async. Perfect for keeping code simple, self contained and to the point.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("David Whitney")]
 [assembly: AssemblyProduct("ReallySimpleEventing")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © David Whitney  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ create-package.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "ReallySimpleEventing.sln"
+.nuget\nuget pack "ReallySimpleEventing.nuspec" -Properties Configuration=Release
