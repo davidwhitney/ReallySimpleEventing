62ce6aed66f2ca71f1d89caf155c94ad2ab20e40
David Whitney <david@davidwhitney.co.uk>
2014-03-11 00:25:18 +0000

    Better waiting in test to support new CI server / ncrunch.

--- ReallySimpleEventing.Test.Unit/ThreadingStrategies/TaskOfTTests.cs
+++ ReallySimpleEventing.Test.Unit/ThreadingStrategies/TaskOfTTests.cs
@@ -12,7 +12,7 @@ namespace ReallySimpleEventing.Test.Unit.ThreadingStrategies
         public void RunTask_CreatesATaskAndStartsIt()
         {
             var tot = new TaskOfT();
-            var task = tot.RunTask(() => Thread.Sleep(1000));
+            var task = tot.RunTask(() => new ManualResetEvent(false).WaitOne(1000));
 
             Assert.That(task, Is.Not.Null);
             Assert.That(task.Status, Is.EqualTo(TaskStatus.Running));
