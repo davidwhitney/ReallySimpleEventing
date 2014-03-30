9bfafcbdf6b38b5f20ec17cea45b02d106586ce0
David Whitney <david@davidwhitney.co.uk>
2014-03-30 02:12:31 +0100

    prevented package building solution and ruining patched version numbers.

--- create-package.bat
+++ create-package.bat
@@ -1,2 +1,1 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "ReallySimpleEventing.sln"
 .nuget\nuget pack "ReallySimpleEventing.nuspec" -Properties Configuration=Release
