c5dc5b89531524066891e3d8267ba9b9ae2e3840
David Whitney <david@davidwhitney.co.uk>
2014-06-23 17:31:25 +0100

    Added azure message bus empty projects.

---
+++ ReallySimpleEventing.MessageBus.Azure/AzureMessageBusPlugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReallySimpleEventing.MessageBus.Azure
+{
+    public class AzureMessageBusPlugin : IReallySimpleEventingRegistrationModule
+    {
+        public void Bootstrap(ReallySimpleEventingConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
