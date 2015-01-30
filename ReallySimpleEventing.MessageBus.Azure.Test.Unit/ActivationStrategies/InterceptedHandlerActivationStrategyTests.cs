f788600b7ae564fb0f6a1fb4e847ffed293b0aaf
David Whitney <david@davidwhitney.co.uk>
2015-01-30 15:58:58 +0000

    Added test for multiple message publishers

--- ReallySimpleEventing.MessageBus.Azure.Test.Unit/ActivationStrategies/InterceptedHandlerActivationStrategyTests.cs
+++ ReallySimpleEventing.MessageBus.Azure.Test.Unit/ActivationStrategies/InterceptedHandlerActivationStrategyTests.cs
@@ -32,12 +32,34 @@ namespace ReallySimpleEventing.MessageBus.Azure.Test.Unit.ActivationStrategies
         }
 
         public class MulticastMessage { }
-
         public class TestSubscription : ISubscribeTo<MulticastMessage>
         {
             public void Handle(MulticastMessage @event) { }
             public void OnError(MulticastMessage @event, Exception ex) { }
         }
+
+        [Test]
+        public void GetHandlers_MultipleSubscriptionsPresent_OnlyOneAzureBusPublisherAdded()
+        {
+            var interceptedStrat = new InterceptedHandlerActivationStrategy(new ActivatorActivation());
+
+            var handlers = interceptedStrat.GetHandlers<MulticastMessageWithManySubscriptions>();
+
+            Assert.That(handlers.Count(x => x.GetType() == typeof(AzureMessageBusPublishingHandler<MulticastMessageWithManySubscriptions>)), Is.EqualTo(1));
+        }
+
+        public class MulticastMessageWithManySubscriptions { }
+        public class TestMultiSubscription1 : ISubscribeTo<MulticastMessageWithManySubscriptions>
+        {
+            public void Handle(MulticastMessageWithManySubscriptions @event) { }
+            public void OnError(MulticastMessageWithManySubscriptions @event, Exception ex) { }
+        }
+        public class TestMultiSubscription2 : ISubscribeTo<MulticastMessageWithManySubscriptions>
+        {
+            public void Handle(MulticastMessageWithManySubscriptions @event) { }
+            public void OnError(MulticastMessageWithManySubscriptions @event, Exception ex) { }
+        }
+
     }
 
 }
